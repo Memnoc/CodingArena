6b9fb8e416892febda2a5d157dd414e0e7d35de6
Memnoc <smartdroidesign@gmail.com>
2023-04-12 08:57:02 +0100

    refactoring

--- js-color-guessing-game/main.js
+++ js-color-guessing-game/main.js
@@ -1,50 +1,58 @@
-import './style.css'
-import { convertToHex } from './create_hex_color.js'
 
-
+import './style.css';
+import { convertToHex } from './create_hex_color.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
-   <h1>Guess the color!</h1>
-   <div class="box-container">
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="card">
+    <h1>Guess the color!</h1>
+    <div class="box-container">
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
     </div>
-    </div>
+    <label id="color-label" type="button"></label>
   </div>
-  <label id="counter" type="button"></label>
-`
+`;
+
 const randomNumber = () => Math.floor(Math.random() * 255);
 const colorBoxes = document.querySelectorAll('.box');
-const colorLabel = document.getElementById('counter');
-
-
-const colors = [];
-
-colorBoxes.forEach(box => {
-  const randomColor = convertToHex(randomNumber(), randomNumber(), randomNumber());
-  box.style.backgroundColor = randomColor;
-  colors.push(randomColor);
-});
-
-const randomLabelColor = colors[Math.floor(Math.random() * colors.length)];
-colorLabel.innerHTML = randomLabelColor;
+const colorLabel = document.getElementById('color-label');
 
 function rgbToHex(rgbString) {
   const values = rgbString.slice(4, -1).split(',').map(Number);
   return `#${values.map(x => x.toString(16).padStart(2, '0')).join('')}`;
 }
 
+function assignColorsToBoxes() {
+  const colors = [];
+
+  colorBoxes.forEach(box => {
+    const randomColor = convertToHex(randomNumber(), randomNumber(), randomNumber());
+    box.style.backgroundColor = randomColor;
+    colors.push(randomColor);
+  });
+
+  return colors;
+}
+
+function setRandomLabelColor(colors) {
+  const randomLabelColor = colors[Math.floor(Math.random() * colors.length)];
+  colorLabel.innerHTML = randomLabelColor;
+}
+
+const colors = assignColorsToBoxes();
+setRandomLabelColor(colors);
+
 colorBoxes.forEach(box => {
   box.addEventListener('click', (e) => {
     const clickedColor = e.target.style.backgroundColor;
     const clickedColorHex = rgbToHex(clickedColor);
-    if (clickedColorHex === randomLabelColor) {
+
+    if (clickedColorHex === colorLabel.innerHTML) {
       colorLabel.innerHTML = 'Correct!';
     } else {
       colorLabel.innerHTML = 'Try again!';
     }
   });
 });
+
